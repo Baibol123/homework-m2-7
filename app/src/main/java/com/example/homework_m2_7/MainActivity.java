edb14e73ec91ee854addd4abdd62aab454d0e55b
baibol123 <baibolchik.002@gmail.com>
2023-11-30 03:59:32 +0600

    change

--- app/src/main/java/com/example/homework_m2_7/MainActivity.java
+++ app/src/main/java/com/example/homework_m2_7/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.homework_m2_7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
@@ -12,20 +14,26 @@ public class MainActivity extends AppCompatActivity{
     private StringBuilder inputStringBuilder;
     private double firstOperand;
     private String pendingOperation;
+    public static final String KEY1 = "key1";
+    public static final String KEY2 = "key2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(getIntent().getBooleanExtra(KEY1, false)) finish();
 
         resultTextView = findViewById(R.id.result_text_view);
         inputStringBuilder = new StringBuilder();
+
     }
 
     public void onDigitClick(View view) {
         String digit = ((TextView) view).getText().toString();
         inputStringBuilder.append(digit);
         updateResultTextView();
+        Button showResultButton = findViewById(R.id.buttonShowResult);
+        showResultButton.setVisibility(View.INVISIBLE);
     }
 
     public void onOperationClick(View view) {
@@ -35,6 +43,8 @@ public class MainActivity extends AppCompatActivity{
             inputStringBuilder.setLength(0);
         }
         pendingOperation = operation;
+        Button showResultButton = findViewById(R.id.buttonShowResult);
+        showResultButton.setVisibility(View.INVISIBLE);
     }
 
     public void onResultClick(View view) {
@@ -64,7 +74,17 @@ public class MainActivity extends AppCompatActivity{
             pendingOperation = null;
             inputStringBuilder.append(firstOperand);
             updateResultTextView();
+            Button showResultButton = findViewById(R.id.buttonShowResult);
+            showResultButton.setVisibility(View.VISIBLE);
         }
+    }
+
+
+
+    public void onGoButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra(KEY2, resultTextView.getText().toString());
+        startActivity(intent);
     }
 
 
@@ -72,6 +92,8 @@ public class MainActivity extends AppCompatActivity{
         if (inputStringBuilder.length() > 0) {
             inputStringBuilder.setLength(0);
             inputStringBuilder.append("0");
+            Button showResultButton = findViewById(R.id.buttonShowResult);
+            showResultButton.setVisibility(View.INVISIBLE);
         }
 
 
@@ -84,4 +106,13 @@ public class MainActivity extends AppCompatActivity{
     private void updateResultTextView() {
         resultTextView.setText(inputStringBuilder.toString());
     }
+
+    public void onShowResultButtonClick(View view) {
+        String result = resultTextView.getText().toString();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("RESULT", result);
+        startActivity(intent);
+    }
+    
 }
